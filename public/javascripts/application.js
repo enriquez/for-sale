e7b8020d3d938446e3a128b67df4e75ba2c8ec8a
Mike Enriquez <mike@enriquez.me>
2010-04-16 20:46:21 -0400

    clean up js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,72 +2,92 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function() {
-    $("#flash-notice").animate({opacity:1}, 4000, 'linear', function() {
-      $("#flash-notice").fadeOut(2000);
-    });
-    $("#flash-error").animate({opacity:1}, 4000, 'linear', function() {
-      $("#flash-error").fadeOut(2000);
-    });
-    $("ul.medium-photos li").hide();
-    $("ul.medium-photos li.active-medium-photo").show();
-    $(".show ul.photos li").each(function(i, p){
-      thumb = $("#" + p.id);
-      thumb.click(function(){
-        $("ul.medium-photos li").hide();
-        medium = $("#medium_" + p.id);
-        medium.show();
-      });
-    });
-    $(".product").each(function(i, p) {
-      product = $("#" + p.id);
-      photo_count = product.children(".photos").children("li").size();
-      if (photo_count < 2) {
-        product.children(".pager").css("visibility", "hidden");
-      }
-      else {
-        left_margin_px = 110 - ((photo_count * 10) / 2);
-        first_dot_element = $(product.children(".pager").children(".dots").children(".first-dot")[0]);
-        first_dot_element.css("margin-left", left_margin_px.toString() + "px");
+  // admin pages
+  $(".flash").flashThenFadeOut();
 
-        i = 1;
-        while (i < photo_count) {
-          first_dot_element.after("<div id=\"" + p.id + "_dot_" + i + "\" class=\"dot\">&nbsp;</div>");
-          i++;
-        }
+  // product show page initialization
+  $("ul.medium-photos li").hide();
+  $("ul.medium-photos li.active-medium-photo").show();
 
-        left_arrow_element = $("#" + p.id + "_left-arrow");
-        right_arrow_element = $("#" + p.id + "_right-arrow");
+  // product show page thumbnail click
+  $(".show ul.photos li").showMediumOnClick();
 
-        left_arrow_element.click(function(){
-          active_dot_element = $("#" + p.id + " .active-dot:first");
-          prev_dot_element = active_dot_element.prev();
-          if (prev_dot_element.size() != 0) {
-            active_dot_element.removeClass("active-dot");
-            prev_dot_element.addClass("active-dot");
-
-            active_photo_element = $("#" + p.id + " ul.photos li.active-photo:first");
-            active_photo_element.removeClass("active-photo");
-            active_photo_element.prev().addClass("active-photo");
-            $("#" + p.id + " ul.photos li").hide();
-            $("#" + p.id + " ul.photos li.active-photo").show();
-          }
-          return false;
-        });
-        right_arrow_element.click(function(){
-          active_dot_element = $("#" + p.id + " .active-dot:first");
-          next_dot_element = active_dot_element.next();
-          if (next_dot_element.size() != 0) {
-            active_dot_element.removeClass("active-dot");
-            next_dot_element.addClass("active-dot");
-
-            active_photo_element = $("#" + p.id + " ul.photos li.active-photo:first");
-            active_photo_element.removeClass("active-photo");
-            active_photo_element.next().addClass("active-photo");
-            $("#" + p.id + " ul.photos li").hide();
-            $("#" + p.id + " ul.photos li.active-photo").show();
-          }
-          return false;
-        });
-      }
-    });
+  // product listing (home page)
+  $(".product").each(function(i, p) {
+    $("#" + p.id).pagePhotos(p.id);
+  });
 });
+
+$.fn.flashThenFadeOut = function() {
+  self = this;
+  this.animate({opacity:1}, 4000, 'linear', function() {
+    self.fadeOut(2000);
+  });
+};
+
+$.fn.showMediumOnClick = function() {
+  this.each(function(i, p) {
+    thumb = $("#" + p.id);
+    thumb.click(function(){
+      $("ul.medium-photos li").hide();
+      medium = $("#medium_" + p.id);
+      medium.show();
+    });
+  });
+}
+
+// direction is 'right' or 'left'
+$.fn.arrowClick = function(product_id, direction) {
+  this.click(function(){
+
+    var arrow;
+    var current_photo;
+    var next_photo;
+    var active_dot_element;
+
+    current_photo = $("#" + product_id + " ul.photos li.active-photo:first");
+    active_dot_element = $("#" + product_id + " .active-dot:first");
+
+    if (direction == 'right') {
+      arrow = active_dot_element.next();
+      next_photo = current_photo.next();
+    }
+    else if (direction == 'left') {
+      arrow = active_dot_element.prev();
+      next_photo = current_photo.prev();
+    }
+
+    if (arrow.size() != 0) {
+      active_dot_element.removeClass("active-dot");
+      arrow.addClass("active-dot");
+
+      current_photo.removeClass("active-photo");
+      next_photo.addClass("active-photo");
+      $("#" + product_id + " ul.photos li").hide();
+      $("#" + product_id + " ul.photos li.active-photo").show();
+    }
+    return false;
+  });
+}
+
+$.fn.pagePhotos = function(product_id) {
+  product = this;
+  photo_count = product.children(".photos").children("li").size();
+  if (photo_count < 2) {
+    product.children(".pager").css("visibility", "hidden");
+  }
+  else {
+    left_margin_px = 110 - ((photo_count * 10) / 2);
+    first_dot_element = $(product.children(".pager").children(".dots").children(".first-dot")[0]);
+    first_dot_element.css("margin-left", left_margin_px.toString() + "px");
+
+    i = 1;
+    while (i < photo_count) {
+      first_dot_element.after("<div id=\"" + product_id + "_dot_" + i + "\" class=\"dot\">&nbsp;</div>");
+      i++;
+    }
+
+    $("#" + product_id + "_left-arrow").arrowClick(product_id, 'left');
+    $("#" + product_id + "_right-arrow").arrowClick(product_id, 'right');
+  }
+};
